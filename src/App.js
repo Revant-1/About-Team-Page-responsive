cb37d24c4cc08b6eeb714d7f1dc6489f590fd9f5
Revant-1 <118761487+Revant-1@users.noreply.github.com>
2023-12-17 16:12:27 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import About3 from './about/About3';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <About3 />
+     {/* <About /> */}
+    </div>
+  );
+}
+
+export default App;
